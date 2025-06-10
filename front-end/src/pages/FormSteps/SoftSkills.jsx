bc4605802312aacc97931586008b23287e4fb263
ZaidRamirez <zramirezh1801@alumno.ipn.mx>
2025-06-10 02:37:21 -0600

    Crear formulario sin estilos

---
+++ front-end/src/pages/FormSteps/SoftSkills.jsx
@@ -0,0 +1,40 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import { Grid, Typography, Slider } from '@mui/material';
+
+const softSkills = [
+    {name: 'trabajoRemoto', label: 'Trabajo remoto'},
+    {name: 'gestionTiempo', label: 'Gestion de tiempo'},
+    {name: 'resolverProblemas', label: 'Resolver problemas'},
+    {name: 'usoHerramientas', label: 'usoHerramientas'},
+    {name: 'comunicacion', label: 'comunicacion'},
+    {name: 'autonomia', label: 'autonomia'},
+];
+
+export default function SoftSkills() {
+  const { control } = useFormContext();
+
+  return (
+    <Grid container spacing={3}>
+      {softSkills.map(({ name, label }) => (
+        <Grid item xs={12} key={name}>
+          <Controller
+            name={name}
+            control={control}
+            render={({ field }) => (
+              <>
+                <Typography gutterBottom>{label}</Typography>
+                <Slider
+                  {...field}
+                  min={1}
+                  max={10}
+                  step={1}
+                  valueLabelDisplay="auto"
+                />
+              </>
+            )}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
